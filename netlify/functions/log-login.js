86b1d4909a8b6aa090e5416468b989db9c434f2a
vivek <vivekbajage55@gmail.com>
2026-06-28 23:34:18 +0530

    Use writable temp storage for Netlify logs

--- netlify/functions/log-login.js
+++ netlify/functions/log-login.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+function getLogFilePath() {
+  if (process.env.NETLIFY || process.env.AWS_LAMBDA_FUNCTION_NAME) {
+    return path.join('/tmp', 'login-attempts.txt');
+  }
+  return path.join(__dirname, '..', '..', 'logs', 'login-attempts.txt');
+}
+
 exports.handler = async function (event, context) {
-  const logDir = path.join(__dirname, '..', '..', 'logs');
-  const logFile = path.join(logDir, 'login-attempts.txt');
+  const logFile = getLogFilePath();
+  const logDir = path.dirname(logFile);
 
   fs.mkdirSync(logDir, { recursive: true });
 
